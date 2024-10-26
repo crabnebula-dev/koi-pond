f1f2e3f161c912d3db1b11f4b1bd042b1d1d5263
timneedham <tim.needham2@gmail.com>
2024-10-26 19:28:45 +0100

    feat: Some ill-advised refactor

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,47 +1,49 @@
-use tauri::{Manager, AppHandle, Emitter};
-use mouse_position::mouse_position::Mouse;
 use mouce::Mouse as OtherMouse;
-use mouce::common::{MouseEvent, MouseButton};
+use mouse_position::mouse_position::Mouse;
+use tauri::{AppHandle, Emitter, Manager};
+// TODO: If I get rid of MouseButton, then no events?
+use mouce::common::{MouseButton, MouseEvent};
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
-  x: i32,
-  y: i32
+    x: i32,
+    y: i32,
 }
 
 fn listen_for_mouse_events(app_handle: AppHandle) {
-  let mut mouse_manager = OtherMouse::new();
+    let mut mouse_manager = OtherMouse::new();
 
-  mouse_manager.hook(Box::new(move |e| {
-    match e{
-      MouseEvent::Press(MouseButton::Left) => {
-        let position = Mouse::get_mouse_position();
-        match position {
-            Mouse::Position { x, y } => {
-                app_handle.emit("mouse_click", Payload {x, y}).expect("Failed to unwrap Mouse position");
-            },
-            Mouse::Error => println!("Error getting mouse position"),
-       }
-      }
-      _ => ()
-    }
-  })).expect("Failed to listen for Mouse Events");
+    mouse_manager
+        .hook(Box::new(move |e| {
+            match e {
+                MouseEvent::AbsoluteMove(x1, y1) => {
+                    app_handle
+                        .emit("mouse_move", Payload { x: *x1, y: *y1 })
+                        .expect("Failed to unwrap Mouse position");
+                }
+                _ => (),
+            }
+        }))
+        .expect("Failed to listen for Mouse Events");
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![])
         .setup(|app| {
             let window = app.get_webview_window("main").expect("Window failed");
-            window.set_ignore_cursor_events(true).expect("Failed to set ignore cursor events");
+            window
+                .set_ignore_cursor_events(true)
+                .expect("Failed to set ignore cursor events");
             window.maximize().expect("Could not maximize window");
 
             let app_handle = app.handle().clone();
             listen_for_mouse_events(app_handle);
             Ok(())
-          })
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,112 @@
+const FPS = 60
+const INTERVAL_MILLISECONDS = 1000 / FPS
+const FRAME_SIZE = 256
+const HALF_FRAME_SIZE = FRAME_SIZE / 2
+const ROTATION_STEP = 15
+const ROTATION_STEP_OFFSET = 6
+const RAD_TO_DEG =  180 / Math.PI
+const MAX_SPEED = 15
+const MIN_SPEED =1
+const ACCELERATION = 0.1
 import "./App.css";
-import { createSignal, onCleanup, createEffect } from "solid-js";
-import { listen } from "@tauri-apps/api/event";
+import {createSignal, onCleanup, createEffect} from "solid-js";
+import {listen} from "@tauri-apps/api/event";
 
-const images = import.meta.glob("./assets/koi-frames/*.png", { eager: true });
+import {info} from '@tauri-apps/plugin-log'
+
+const IMAGES = import.meta.glob("./assets/koi-frames/*.png", {eager: true});
+
 
 
 function App() {
-  let koiRef: HTMLDivElement;
+
+  function convertRadsToDirection(rads: number) {
+    let direction = rads  * RAD_TO_DEG
+    const remainder = direction % ROTATION_STEP
+    direction -= remainder
+    if (remainder > ROTATION_STEP_OFFSET) direction += ROTATION_STEP
+    while (direction < 0 || direction >= 360) {
+      if (direction < 0) {
+        direction += 360
+      } else {
+        direction -= 360
+      }
+    }
+    return direction
+  }
 
   type Position = {
     x: number;
     y: number;
   };
 
-  const homePosition = { x: 0, y: 0 };
+  let frameCounter = MIN_SPEED
+  let speed = MIN_SPEED
+  let heading = 0
+  const homePosition = {x: 500, y: 500};
 
-  const [position, setPosition] = createSignal(homePosition);
-  const [direction, setDirection] = createSignal(0);
+  const [fishPosition, setFishPosition] = createSignal(homePosition);
+  const [targetPosition, setTargetPosition] = createSignal(homePosition);
   const [frame, setFrame] = createSignal(1);
+  const [direction, setDirection] = createSignal(0);
 
-  const incrementNumber = () => setFrame((prev) => (prev % 12) + 1);
+  function calculateNewRelativePosition(angleRad: number, length: number, xOffset: number, yOffset: number): Position {
+    return {
+      x: (length * Math.cos(angleRad - Math.PI)) + xOffset,
+      y: (length * Math.sin(angleRad - Math.PI)) + yOffset
+    };
+  }
 
-  createEffect(() => {const interval = setInterval(incrementNumber, 50);
+  const processFrame = () => {
+    const {x: targetX, y: targetY} = targetPosition()
+    const {x: originalFishX, y: originalFishY} = fishPosition()
+    const w = originalFishX - targetX;
+    const h = originalFishY - targetY;
+    const angleRad = Math.atan2(h, w);
 
-  onCleanup(() => clearInterval(interval));
+    if (angleRad > heading) heading-= 0.5
+    if (angleRad < heading) heading+= 0.5
+
+    const direction = convertRadsToDirection(angleRad)
+    const distance = Math.hypot(w, h)
+    if (distance > MAX_SPEED) {
+      if (speed<MAX_SPEED) {
+        speed+=ACCELERATION
+        setDirection(direction);
+      }
+    } else {
+      if (speed>MIN_SPEED) {
+        speed = MIN_SPEED
+      }
+    }
+
+    const newPosition = calculateNewRelativePosition(angleRad, speed, originalFishX, originalFishY)
+    setFishPosition(newPosition)
+    frameCounter-= 1
+    if (frameCounter < 1) {
+      frameCounter = 5 - (speed / 3)
+      setFrame((prev) => (prev % 12) + 1)
+    }
+  };
+
+  createEffect(() => {
+    const interval = setInterval(processFrame, INTERVAL_MILLISECONDS);
+    onCleanup(() => clearInterval(interval));
   });
 
-  listen<Position>("mouse_click", (event) => {
-    if (sendHome(event.payload)) return;
-
-    calculateDirection(position(), event.payload);
-    setPosition(event.payload);
+  listen<Position>("mouse_move", (event) => {
+    setTargetPosition(event.payload);
   });
-
-  const sendHome = (clickPosition: Position) => {
-    if (!koiRef) return false;
-
-    const boundingBox = koiRef.getBoundingClientRect();
-    if (!boundingBoxIsClicked(boundingBox, clickPosition)) return false;
-
-    calculateDirection(position(), homePosition);
-    setPosition(homePosition);
-    return true;
-  };
-
-  const boundingBoxIsClicked = (
-    boundingBox: DOMRect,
-    clickPosition: Position
-  ) => {
-    const { x, y } = clickPosition;
-
-    return (
-      x > boundingBox.left &&
-      x < boundingBox.right &&
-      y > boundingBox.top &&
-      y < boundingBox.bottom
-    );
-  };
-
-  const calculateDirection = (oldPosition: Position, newPosition: Position) => {
-    const x = oldPosition.x - newPosition.x;
-    const y = oldPosition.y - newPosition.y;
-    let angle = (Math.atan2(y, x) * 180) / Math.PI;
-
-    if (angle < 0) angle = 360 + angle;
-    
-    const roundToNearest15 = Math.round(angle / 15) * 15;
-    roundToNearest15  === 360 ? 0 : roundToNearest15;
-    setDirection(roundToNearest15);
-  };
 
   const calculateStyle = (position: Position) => {
-    return `position: absolute; left: ${position.x - 50}px; top: ${
-      position.y - 80
-    }px; 
-    animation-timing-function: ease-in-out;
-    transition: top 1.5s, left 1.5s, transform 0.5s;
-  `;
+    return `position: absolute; left: ${position.x - HALF_FRAME_SIZE}px; top: ${position.y - HALF_FRAME_SIZE}px;`;
   };
 
   return (
     <div class="container">
-      <div style={calculateStyle(position())} ref={koiRef}>
-        <img class="koi" width={175} src={images[`./assets/koi-frames/koi-${direction().toString().padStart(3, '0')}-${frame().toString().padStart(2, '0')}.png`].default} />
+      <div style={calculateStyle(fishPosition())}>
+        <img class="koi" width={FRAME_SIZE}
+             src={IMAGES[`./assets/koi-frames/koi-${direction().toString().padStart(3, '0')}-${frame().toString().padStart(2, '0')}.png`].default}/>
       </div>
     </div>
   );
